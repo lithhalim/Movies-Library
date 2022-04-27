8d56a61162116b9b487fdd4a2201c172df11ef49
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-04-27 16:26:34 +0300

    lith

--- index.js
+++ index.js
@@ -1,24 +1,92 @@
 const express = require('express')
+const axios = require('axios').default;
 const app = express()
 const dataJson=require('./Movie Data/data.json')
 const port = 3000
 const User=new Select(dataJson.title,dataJson.poster_path,dataJson.overview)
+
+
+require('dotenv').config()
+const apiKey=process.env.SECRET_KEY
+
+
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.json(User)
 })
 app.get('/favorite', (req, res) => {
     res.send('Welcome to Favorite Page')
+})
+app.get('/trending', (req, res) => {
+    let url=`https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}&language=en-US`
+    axios.get(url)
+  .then(function (response) {
+    // handle success
+    let data=(response.data.results.find((a)=>(a.id=="634649")));
+    let Doto=new Trindong(data.id,data.title,data.release_date,data.poster_path,data.overview)
+    
+    res.json(Doto)
+
+  })
+  .catch(function (error) {
+  })
 
 })
+
+app.get('/search', (req, res) => {
+    let quer=req.query.query
+    console.log(quer)
+    let url=`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${quer}&page=2`
+
+    axios.get(url)
+  .then(function (response) {
+      res.json(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 app.listen(port, () => {
-    console.log(dataJson)
 
   console.log(`Example app listening on port ${port}`)
 })
-
+//////////constructor for select
 function Select(title,poster_path,overview){
     this.title=title,
+    this.poster_path=poster_path,
+    this.overview=overview
+}
+
+
+///////////////constructor for trinding
+function Trindong(id,title,release_date,poster_path,overview){
+    this.id=id,
+    this.title=title,
+    this.release_date=release_date,
     this.poster_path=poster_path,
     this.overview=overview
 }
